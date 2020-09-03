35479e32583265a221c186fc60c708081e49c793
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-09-03 16:31:41 +0300

    add index page to heroku

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 connectDB();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.render(path.join(__dirname, "public/index.html"));
 });
 app.use(bodyParser.json());
 
